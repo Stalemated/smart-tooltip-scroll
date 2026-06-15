717b9d784340e2d5829734cb036d87d3cad7a5fe
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-06-15 14:54:06 -0600

    Fix: correctly annotated SmartTooltipScrollFabric.java as client side

--- fabric/src/main/java/com/stalemated/sts/fabric/SmartTooltipScrollFabric.java
+++ fabric/src/main/java/com/stalemated/sts/fabric/SmartTooltipScrollFabric.java
@@ -3,8 +3,11 @@ package com.stalemated.sts.fabric;
 import net.fabricmc.api.ClientModInitializer;
 
 import com.stalemated.sts.SmartTooltipScrollClient;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
+@Environment(EnvType.CLIENT)
 public final class SmartTooltipScrollFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
