2f57922e46e57cebe87d5b5ca51f5d1a69554d27
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-06-15 15:49:41 -0600

    Feat: added a reference to a global boolean using a state manager to determine if a tooltip needs to be scrolled (if it's not an item), directly fixes compat with CTA

--- common/src/main/java/com/stalemated/sts/resize/TooltipDimensionManager.java
+++ common/src/main/java/com/stalemated/sts/resize/TooltipDimensionManager.java
@@ -2,6 +2,7 @@ package com.stalemated.sts.resize;
 
 import com.stalemated.lib.util.math.MathUtils;
 import com.stalemated.lib.helper.PlatformHelper;
+import com.stalemated.lib.util.state.SharedTooltipState;
 import com.stalemated.lib.util.style.TooltipStyleUtils;
 import com.stalemated.sts.compat.LegendaryTooltipsCompat;
 import com.stalemated.sts.config.ConfigManager;
@@ -69,7 +70,7 @@ public class TooltipDimensionManager {
     }
 
     public static List<Text> enforceWidthLimit(List<Text> text) {
-        isCurrentTooltipItemTooltip = nextTooltipIsItem;
+        isCurrentTooltipItemTooltip = SharedTooltipState.forceCustomDimensions || nextTooltipIsItem;
         nextTooltipIsItem = false;
 
         if (!config.custom_tooltip_dimensions || !isCurrentTooltipItemTooltip || text.isEmpty()) {
