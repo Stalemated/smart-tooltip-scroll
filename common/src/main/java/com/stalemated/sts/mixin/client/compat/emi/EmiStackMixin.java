dc6cd0b0bda9ad7026ee98fe44196ce2de95aed7
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-06-17 22:49:59 -0600

    Fix: fixed the plugin not being applied properly

--- common/src/main/java/com/stalemated/sts/mixin/client/compat/emi/EmiStackMixin.java
+++ common/src/main/java/com/stalemated/sts/mixin/client/compat/emi/EmiStackMixin.java
@@ -3,6 +3,7 @@ package com.stalemated.sts.mixin.client.compat.emi;
 import com.stalemated.sts.compat.emi.EmiItemMarkerComponent;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -10,10 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(targets = "dev.emi.emi.api.stack.ItemEmiStack", remap = false)
+@Pseudo
+@Mixin(targets = "dev.emi.emi.api.stack.ItemEmiStack")
 public class EmiStackMixin {
 
-    @Inject(method = "getTooltip", at = @At("RETURN"), cancellable = true, remap = false)
+    @Inject(method = "getTooltip", at = @At("RETURN"), cancellable = true)
     private void sts$injectMarker(CallbackInfoReturnable<List<TooltipComponent>> cir) {
         List<TooltipComponent> list = cir.getReturnValue();
 
