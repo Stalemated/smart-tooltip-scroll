bd1b42b857adeb1bcb3f30193eef95a3d4a03705
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-24 13:45:38 -0600

    Feat: update s-lib dependency and with it implemented a new, unified way to register config files

--- common/src/main/java/com/stalemated/sts/config/ConfigManager.java
+++ common/src/main/java/com/stalemated/sts/config/ConfigManager.java
@@ -1,22 +1,19 @@
 package com.stalemated.sts.config;
 
-import com.stalemated.lib.helper.PlatformHelper;
+import com.stalemated.lib.config.BaseConfigManager;
+import com.stalemated.lib.config.ConfigProvider;
 import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import net.minecraft.util.Identifier;
 
-import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 import static com.stalemated.sts.SmartTooltipScrollClient.LOGGER;
 
 public class ConfigManager {
 
-    private static final Path CONFIG_PATH = PlatformHelper.INSTANCE.getConfigDir().resolve("smart_tooltip_scroll.json5");
-    public static boolean configLoadFailed = false;
-
+    private static final Path CONFIG_PATH = BaseConfigManager.buildPath("smart_tooltip_scroll.json5");
+    
     public static final ConfigClassHandler<STSConfig> HANDLER = ConfigClassHandler.createBuilder(STSConfig.class)
             .id(new Identifier("sts", "config"))
             .serializer(config -> GsonConfigSerializerBuilder.create(config)
@@ -25,39 +22,39 @@ public class ConfigManager {
                     .build())
             .build();
 
+    private static final BaseConfigManager<STSConfig> CONFIG = new BaseConfigManager<STSConfig>(
+            new ConfigProvider<STSConfig>() {
+                @Override
+                public boolean load() {
+                    return HANDLER.load();
+                }
+
+                @Override
+                public void save() {
+                    HANDLER.save();
+                }
+
+                @Override
+                public STSConfig instance() {
+                    return HANDLER.instance();
+                }
+            },
+            CONFIG_PATH,
+            LOGGER
+    ) {};
+
+    public static boolean configLoadFailed = false;
+
     public static void register() {
-        File configFile = CONFIG_PATH.toFile();
-        boolean isNewOrEmpty = !configFile.exists() || configFile.length() == 0;
-
-        if (configFile.exists() && configFile.length() == 0) {
-            try {
-                boolean ignored = configFile.delete();
-            } catch (Exception e) {
-                LOGGER.warn("Failed to delete empty config file: ", e);
-            }
-        }
-
-        boolean loaded = HANDLER.load();
-
-        if (!loaded && !isNewOrEmpty) {
-            configLoadFailed = true;
-            File configBackup = CONFIG_PATH.getParent().resolve("smart_tooltip_scroll_backup.json5").toFile();
-            try {
-                Files.copy(configFile.toPath(), configBackup.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                LOGGER.error("A backup of your broken config was saved to: {}", configBackup.getName());
-            } catch (Exception e) {
-                LOGGER.error("Failed to create backup of the broken config!", e);
-            }
-        }
-
-        if (isNewOrEmpty) save();
+        CONFIG.register();
+        configLoadFailed = CONFIG.configLoadFailed;
     }
 
     public static STSConfig getConfig() {
-        return HANDLER.instance();
+        return CONFIG.getConfig();
     }
 
     public static void save() {
-        HANDLER.save();
+        CONFIG.save();
     }
 }
